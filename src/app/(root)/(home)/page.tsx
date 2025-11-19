3ef04baa9ebcbe30aeaaf26805fcd6e2471f9b71
Pawel <pawel.zaremba013@gmail.com>
2025-11-19 21:04:27 +0100

    Finish Stream Provider Setup

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -10,6 +10,7 @@ import {useQuery} from "convex/react";
 import {api} from "../../../../convex/_generated/api";
 import {useRouter} from "next/navigation";
 import MeetingModal from "@/components/MeetingModal";
+import LoaderUI from "@/components/LoaderUI";
 
 export default function Home() {
     const router = useRouter()
@@ -19,7 +20,7 @@ export default function Home() {
     const [showModal, setShowModal] = useState(false)
     const [modalType, setModalType] = useState<"start" | "join">()
 
-    if (isLoading) return <p>Loading...</p>
+    if (isLoading) return <LoaderUI />
 
     const handleQuickAction = (title: string) => {
         switch (title) {
---
+++ src/components/LoaderUI.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {LoaderIcon} from "lucide-react";
+
+function LoaderUi() {
+    return (
+        <div className="h-[calc(100vh-4rem-1px)] flex items-center justify-center">
+            <LoaderIcon className="size-8 animate-spin text-muted-foreground"/>
+        </div>
+    )
+}
+
+export default LoaderUi
