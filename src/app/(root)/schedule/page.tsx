db6f15043d3b38c304e12435751ff6b4a5f636e6
Pawel <pawel.zaremba013@gmail.com>
2025-11-22 21:51:37 +0100

    Finish Schedule Page

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+"use client"
+
+import {useRouter} from "next/navigation";
+import {useUserRole} from "@/hooks/useUserRole";
+import LoaderUI from "@/components/LoaderUI";
+import InterviewScheduleUI from "@/app/(root)/schedule/InterviewScheduleUI";
 
 function SchedulePage() {
+    const router = useRouter()
+
+    const { isInterviewer, isLoading } = useUserRole()
+
+    if (isLoading) return <LoaderUI />
+    if (!isInterviewer) return router.push("/")
     return (
-        <div>SchedulePage</div>
+        <InterviewScheduleUI />
     )
 }
 
